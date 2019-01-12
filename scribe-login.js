a89dcd513b126f5030e89caaba9e69c4b6dc1639
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-01-12 17:46:31 -0500

    added optional email/password flags to login command

--- scribe-login.js
+++ scribe-login.js
@@ -7,7 +7,10 @@ require('dotenv')
 
 const ENDPOINT = 'http://138.197.130.167/scribe'
 
-program.parse(process.argv);
+program
+  .option('-e, --email <argument>', 'Enter email as argument')
+  .option('-p --password <argument>', 'Enter password as argument')
+  .parse(process.argv);
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -49,6 +52,15 @@ getPassword = () => {
     })
 }
 
+loginMutation = (email, password) => {
+    return {
+        "query": `
+        mutation loginMutation($email: String!, $password: String!, $rememberMe: Boolean!) { login(email: $email, password: $password, rememberMe: $rememberMe) { token } }
+        `,
+        variables: { email: `${email}`, password: `${password}`, rememberMe: false }
+    }
+}
+
 rp = () => {
     post = (body) => {
         return requestPromise(ENDPOINT, {
@@ -65,44 +77,58 @@ rp = () => {
     }
 }
 
-getEmail().then((emailInput) => {
-    const email = emailInput
-    getPassword().then(function(passwordInput) {
-        const password = passwordInput
-
-        const loginMutation = {
-            "query": `
-                mutation loginMutation($email: String!, $password: String!, $rememberMe: Boolean!) { login(email: $email, password: $password, rememberMe: $rememberMe) { token } }
-            `,
-            variables: { email: `${email}`, password: `${password}`, rememberMe: false }
+if(program.email !== null && program.password !== null) {
+    rp().post(loginMutation(program.email, program.password)).then(res => {
+        const { login } = res.data
+        if(login !== null) {
+            console.log()
+            console.log("\n****************************************************************************")
+            console.log("\t\t      SCRIBE ACCESS TOKEN")
+            console.log("****************************************************************************")
+            console.log(login.token)
+            console.log()
+            process.exit(1)
+        } else {
+            console.log("\nIncorrect Username or Password. Try again.\n")
+            process.exit(1)
         }
-
-        rp().post(loginMutation).then(res => {
-            const { login } = res.data
-            if(login !== null) {
-                console.log()
-                console.log("\n****************************************************************************")
-                console.log("\t\t      SCRIBE ACCESS TOKEN")
-                console.log("****************************************************************************")
-                console.log(login.token)
-                console.log()
-            } else {
-                console.log("\nIncorrect Username or Password. Try again.\n")
-                process.exit(1)
-            }
+    }).catch((err) => {
+        console.error(err)
+    })
+} else {
+    getEmail().then((emailInput) => {
+        const email = emailInput
+        getPassword().then(function(passwordInput) {
+            const password = passwordInput
+            rp().post(loginMutation(email, password)).then(res => {
+                const { login } = res.data
+                if(login !== null) {
+                    console.log()
+                    console.log("\n****************************************************************************")
+                    console.log("\t\t      SCRIBE ACCESS TOKEN")
+                    console.log("****************************************************************************")
+                    console.log(login.token)
+                    console.log()
+                    process.exit(1)
+                } else {
+                    console.log("\nIncorrect Username or Password. Try again.\n")
+                    process.exit(1)
+                }
+            }).catch((err) => {
+                console.error(err)
+            })
         }).catch((err) => {
+            console.log()
             console.error(err)
+            process.exit(1)
         })
     }).catch((err) => {
         console.log()
         console.error(err)
         process.exit(1)
     })
-}).catch((err) => {
-    console.log()
-    console.error(err)
-    process.exit(1)
-})
+}
+
 
 
 
