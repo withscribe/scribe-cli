496a4f485602b60934dda3b03eeab8dade359813
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-01-14 17:30:00 -0500

    added story command (partial implementation)

--- scribe.js
+++ scribe.js
@@ -3,10 +3,10 @@
 
 const program = require('commander');
 
+// TODO: refactor stories to mystories or something (only allow to pull user's own stories)
 program
 .version('0.0.1')
 .command('login','get access token').alias('l')
-.command('search <query>', 'searches with argument as filter').alias('s')
-.command('stories <count>','returns a list of public stories')
+.command('stories','returns a list of public stories').alias('s')
 .parse(process.argv);
 
